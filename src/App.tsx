771b70122e712be20bf6572ece865d5a82f90111
marbin <marvin.dev76@gmail.com>
2026-03-08 12:47:42 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Container, Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Container maxWidth="6xl">
+      <Heading>Hello!</Heading>
+    </Container>
+  );
+}
+
+export default App;
